84c5b50a79449ebac664fbb0d96c7846cacf7ecd
JAMES CURLEY - Student <G00262917@gmit.ie>
2020-02-03 09:33:35 +0000

    First commit in my Calculator repo

---
+++ src/main/java/ie/gmit/Calculator.java
@@ -0,0 +1,11 @@
+package ie.gmit;
+
+public class Calculator {
+
+    public static int addition(int firstNum, int secondNum) {
+        
+        int sum = firstNum + secondNum;
+        
+        return sum;
+    }
+}
